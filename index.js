42da50d8f551c98eaa8f4fcb5a4cfcc162993e44
Lev Litvinov <misery7100@gmail.com>
2023-09-09 23:45:52 +0300

    Fix typo

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ async function addItem(title, message, time, committer, notionTaskId) {
                         start: time
                     }
                 },
-                "Committed by": {
+                "Commited by": {
                     rich_text: [
                         {
                             text: {
